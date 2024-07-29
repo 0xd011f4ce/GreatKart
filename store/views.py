cc678906e9e44b14e76627dc9692b9fb895b91a8
lain <lyx@lyxincyberia.org>
2024-07-29 16:17:00 -0500

    display products by category

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from category.models import Category
 from . import models
 
-def store (request):
-    products = models.Product.objects.all ().filter (is_available = True)
-    total_products = products.count ()
+def store (request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        # Get the category by slug and filter products by category
+        categories = get_object_or_404 (Category, slug=category_slug)
+        products = models.Product.objects.filter (category=categories, is_available=True)
+        total_products = products.count ()
+    else:
+        products = models.Product.objects.all ().filter (is_available = True)
+        total_products = products.count ()
 
     ctx = {
         "products": products,
